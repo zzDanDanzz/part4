06cf8c7aaafaf4b1bc0c2844500c8e8ecb769de4
zzDanDanzz <94631874+zzDanDanzz@users.noreply.github.com>
2021-12-24 23:40:01 +0330

    'controllers' and 'models' modules working

--- index.js
+++ index.js
@@ -4,15 +4,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config()
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const blogsRouter = require('./controllers/blogs')
 
 const mongoUrl = process.env.MONGO_URI
 mongoose.connect(mongoUrl).then(res => console.log('connected to db'))
@@ -20,25 +12,9 @@ mongoose.connect(mongoUrl).then(res => console.log('connected to db'))
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
