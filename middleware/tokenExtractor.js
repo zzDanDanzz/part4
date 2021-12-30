06220000707d47d5287080a4353bce1d8b60d174
zzDanDanzz <dansfancymaths@gmail.com>
2021-12-31 02:14:22 +0330

    bug fix
    now gettokenfrom function doesn't interfere with requests that don't need auth

--- middleware/tokenExtractor.js
+++ middleware/tokenExtractor.js
@@ -2,11 +2,15 @@ const { AuthError } = require('../utils/verification')
 
 const getTokenFrom = (req, res, next) => {
   const authHeader = req.get('authorization')
-  if (authHeader && authHeader.includes('bearer ')) {
+  if (!authHeader) {
+    next()
+    return null
+  }
+  if (authHeader.includes('bearer ')) {
     req.token = authHeader.substring(7)
     next()
   } else {
-    throw new AuthError('token required in header')
+    throw new AuthError('token missing or invalid')
   }
 }
 
