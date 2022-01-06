646b17295c0ae66e5936a337ac3b4000973bb92b
zzDanDanzz <94631874+zzDanDanzz@users.noreply.github.com>
2022-01-06 17:57:10 +0330

    fix testing.js

--- controllers/testing.js
+++ controllers/testing.js
@@ -2,9 +2,11 @@ const testingRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-testingRouter.post('/reset', async () => {
+testingRouter.post('/reset', async (req, res) => {
   await Blog.deleteMany()
   await User.deleteMany()
 
-  module.exports = testingRouter
+  res.status(204).end()
 })
+
+module.exports = testingRouter
